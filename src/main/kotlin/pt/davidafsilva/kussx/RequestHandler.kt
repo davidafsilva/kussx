478c241e320dd53bb5e318ac5be4e528c807af19
David Silva <david@davidafsilva.pt>
2017-10-04 18:05:51 +0100

    Rest API server implementation

---
+++ src/main/kotlin/pt/davidafsilva/kussx/RequestHandler.kt
@@ -0,0 +1,123 @@
+package pt.davidafsilva.kussx
+
+import io.netty.handler.codec.http.HttpResponseStatus
+import io.vertx.core.AsyncResult
+import io.vertx.core.Handler
+import io.vertx.core.http.HttpHeaders
+import io.vertx.core.json.JsonObject
+import io.vertx.redis.RedisOptions
+import io.vertx.rxjava.core.Vertx
+import io.vertx.rxjava.ext.web.RoutingContext
+import io.vertx.rxjava.redis.RedisClient
+import org.slf4j.LoggerFactory
+import pt.davidafsilva.hashids.Hashids
+import rx.Single
+import rx.schedulers.Schedulers
+import java.security.SecureRandom
+import java.time.Instant
+import java.util.concurrent.Executors
+
+/**
+ * The API request handler
+ *
+ * @author david
+ */
+class RequestHandler(vertx: Vertx) {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
+    private val incrExecutor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() * 4)
+
+    private var hashids: Hashids? = null
+    private var redisClient: RedisClient? = null
+
+    init {
+        val config = vertx.orCreateContext.config()
+        // hashids
+        val salt = config.map.computeIfAbsent("KUSSX_SALT", {
+            val b = ByteArray(16)
+            SecureRandom().nextBytes(b)
+            logger.debug("used {} as salt", b)
+            b.toString()
+        }) as String
+        hashids = Hashids.newInstance(salt)
+
+        // redis client
+        redisClient = RedisClient.create(vertx, RedisOptions().apply {
+            this.auth = config.getString("KUSSX_REDIS_AUTH_PASSWORD")
+            this.host = config.getString("KUSSX_REDIS_HOST", "localhost")
+            this.port = config.getInteger("KUSSX_REDIS_PORT", 6379)
+        })
+    }
+
+    fun handleIdDecodeRequest(context: RoutingContext) {
+        val key = context.pathParam("key")
+        redisClient?.run {
+            rxGet(key)
+                    .flatMap {
+                        if (it == null) Single.error(RuntimeException("no suck mapping"))
+                        else Single.just(it)
+                    }
+                    .map { JsonObject(it) }
+                    .doOnSuccess {
+                        // redirect the request
+                        context.response()
+                                .setStatusCode(HttpResponseStatus.TEMPORARY_REDIRECT.code())
+                                .putHeader(HttpHeaders.LOCATION.toString(), it.getString("url", ""))
+                                .end()
+
+                        // increment the access counter in the backgroun
+                        rxSet(key, it.put("access", it.getLong("access", 0L) + 1).encode())
+                                .subscribeOn(Schedulers.from(incrExecutor))
+                                .subscribe()
+                    }
+                    .doOnError { context.fail(HttpResponseStatus.NOT_FOUND.code()) }
+                    .subscribe()
+        }
+    }
+
+    fun handleIdCreationRequest(context: RoutingContext) {
+        val url: String? = context.bodyAsJson.getString("url", "")
+        when {
+            url.isNullOrEmpty() -> context.fail(HttpResponseStatus.BAD_REQUEST.code())
+            else -> {
+                redisClient?.apply {
+                    rxIncr("shorten-counter")
+                            .map { hashids?.encode(it) }
+                            .flatMap { k -> rxExists(k).map { Pair(it, k) } }
+                            .flatMap {
+                                if (it.first == 0L) Single.just(it.second)
+                                else Single.error(DuplicateKeyException())
+                            }
+                            .retry { _, e -> e is DuplicateKeyException }
+                            .flatMap { k ->
+                                val json = JsonObject()
+                                        .put("url", url)
+                                        .put("created", Instant.now())
+                                        .put("access", 0L)
+                                        .encode()
+                                rxSet(k, json).map { JsonObject().put("key", k) }
+                            }
+                            .doOnSuccess { context.response().end(it.encode()) }
+                            .doOnError { context.fail(HttpResponseStatus.INTERNAL_SERVER_ERROR.code()) }
+                            .subscribe()
+                }
+            }
+        }
+    }
+
+    fun handleIdDeletionRequest(context: RoutingContext) {
+        val key = context.pathParam("key")
+        redisClient?.run {
+            rxDel(key)
+                    .doOnSuccess { context.response().end() }
+                    .doOnError { context.fail(HttpResponseStatus.INTERNAL_SERVER_ERROR.code()) }
+                    .subscribe()
+        }
+    }
+
+    fun close(completer: Handler<AsyncResult<Void>>?) {
+        redisClient?.close(completer)
+    }
+
+    private class DuplicateKeyException : RuntimeException()
+}
---
+++ src/main/kotlin/pt/davidafsilva/kussx/RestVerticle.kt
@@ -0,0 +1,54 @@
+package pt.davidafsilva.kussx
+
+import io.vertx.core.Future
+import io.vertx.core.http.HttpServerOptions
+import io.vertx.rxjava.core.AbstractVerticle
+import io.vertx.rxjava.ext.web.Router
+import io.vertx.rxjava.ext.web.handler.BodyHandler
+import org.slf4j.LoggerFactory
+
+/**
+ * This verticle shall expose our REST API and inter-op with our data store, accordingly.
+ *
+ * @author david
+ */
+class RestVerticle : AbstractVerticle() {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
+    private var requestHandler: RequestHandler? = null
+
+    override fun start(startFuture: Future<Void>?) {
+        // request handler
+        requestHandler = RequestHandler(vertx)
+
+        // options
+        val options = HttpServerOptions().apply {
+            port = config().getInteger("KUSSX_API_PORT", 8080)
+            host = config().getString("KUSSX_API_HOST", "0.0.0.0")
+        }
+        // routing
+        val router = Router.router(vertx).also {
+            it.get("/:key").handler(requestHandler!!::handleIdDecodeRequest)
+            it.post("/shorten").handler(BodyHandler.create())
+            it.post("/shorten")
+                    .consumes("application/json")
+                    .produces("application/json")
+                    .handler(requestHandler!!::handleIdCreationRequest)
+            it.delete("/:key")
+                    .handler(requestHandler!!::handleIdDeletionRequest)
+        }
+        // create http server
+        vertx.createHttpServer(options)
+                .requestHandler(router::accept)
+                .listen({ result ->
+                    if (result.succeeded()) {
+                        logger.info("API server deployed at {}:{}", options.host, result.result().actualPort())
+                        startFuture?.complete()
+                    } else startFuture?.fail(result.cause())
+                })
+    }
+
+    override fun stop(stopFuture: Future<Void>?) {
+        requestHandler?.close(stopFuture?.completer())
+    }
+}
