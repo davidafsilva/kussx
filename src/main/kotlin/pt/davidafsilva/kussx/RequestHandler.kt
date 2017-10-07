9be3181ad4e197d3d1c218fdb57131ac089dfbef
David Silva <david@davidafsilva.pt>
2017-10-07 12:43:04 +0100

    removed unncessary onErrorNotImplemented related stacktraces

--- src/main/kotlin/pt/davidafsilva/kussx/RequestHandler.kt
+++ src/main/kotlin/pt/davidafsilva/kussx/RequestHandler.kt
@@ -3,7 +3,7 @@ package pt.davidafsilva.kussx
 import io.netty.handler.codec.http.HttpResponseStatus.*
 import io.vertx.core.AsyncResult
 import io.vertx.core.Handler
-import io.vertx.core.http.HttpHeaders.LOCATION
+import io.vertx.core.http.HttpHeaders
 import io.vertx.core.json.JsonObject
 import io.vertx.redis.RedisOptions
 import io.vertx.rxjava.core.Vertx
@@ -50,45 +50,26 @@ class RequestHandler(vertx: Vertx, private val config: Configuration) {
     }
 
     fun keyInformation(context: RoutingContext) {
-        val key = context.pathParam("key")
-        redisClient?.run {
-            rxGet(key)
-                    .flatMap {
-                        if (it == null) Single.error(RuntimeException("no such key"))
-                        else Single.just(JsonObject(it))
-                    }
-                    .doOnSuccess {
-                        context.response().end(it.encode())
-                    }
-                    .doOnError { context.fail(NOT_FOUND.code()) }
-                    .subscribe()
-        }
+        getKey(context.pathParam("key")).subscribe(
+                { context.response().end(it.encode()) },
+                { logger.error("unable obtain key", it); context.fail(NOT_FOUND.code()) }
+        )
     }
 
     fun redirectWithKey(context: RoutingContext) {
         val key = context.pathParam("key")
-        redisClient?.run {
-            rxGet(key)
-                    .flatMap {
-                        if (it == null) Single.error(RuntimeException("no such key"))
-                        else Single.just(it)
-                    }
-                    .map { JsonObject(it) }
-                    .doOnSuccess {
-                        // redirect the request
-                        context.response()
-                                .setStatusCode(TEMPORARY_REDIRECT.code())
-                                .putHeader(LOCATION.toString(), it.getString("url", ""))
-                                .end()
+        getKey(key).subscribe({
+            // redirect the request
+            context.response()
+                    .setStatusCode(TEMPORARY_REDIRECT.code())
+                    .putHeader(HttpHeaders.LOCATION.toString(), it.getString("url", ""))
+                    .end()
 
-                        // increment the access counter in the background
-                        rxSet(key, it.put("access", it.getLong("access", 0L) + 1).encode())
-                                .subscribeOn(Schedulers.from(incrExecutor))
-                                .subscribe()
-                    }
-                    .doOnError { context.fail(NOT_FOUND.code()) }
+            // increment the access counter in the background
+            redisClient!!.rxSet(key, it.put("access", it.getLong("access", 0L) + 1).encode())
+                    .subscribeOn(Schedulers.from(incrExecutor))
                     .subscribe()
-        }
+        }, { logger.error("unable redirect key", it); context.fail(NOT_FOUND.code()) })
     }
 
     fun createKey(context: RoutingContext) {
@@ -97,14 +78,17 @@ class RequestHandler(vertx: Vertx, private val config: Configuration) {
             url.isNullOrEmpty() -> context.fail(BAD_REQUEST.code())
             else -> {
                 redisClient?.apply {
+                    // get the current counter value and encode it
                     rxIncr("shorten-counter")
-                            .map { hashids?.encode(it) }
+                            .map { hashids!!.encode(it) }
+                            // check for duplicates (misread counters) and retry
                             .flatMap { k -> rxExists(k).map { Pair(it, k) } }
                             .flatMap {
                                 if (it.first == 0L) Single.just(it.second)
                                 else Single.error(DuplicateKeyException())
                             }
                             .retry { _, e -> e is DuplicateKeyException }
+                            // create the new mapping
                             .flatMap { k ->
                                 val json = JsonObject()
                                         .put("url", url)
@@ -113,9 +97,13 @@ class RequestHandler(vertx: Vertx, private val config: Configuration) {
                                         .encode()
                                 rxSet(k, json).map { JsonObject().put("key", k) }
                             }
-                            .doOnSuccess { context.response().end(it.encode()) }
-                            .doOnError { context.fail(INTERNAL_SERVER_ERROR.code()) }
-                            .subscribe()
+                            .subscribe(
+                                    { context.response().end(it.encode()) },
+                                    {
+                                        logger.error("unable create key", it)
+                                        context.fail(INTERNAL_SERVER_ERROR.code())
+                                    }
+                            )
                 }
             }
         }
@@ -124,15 +112,26 @@ class RequestHandler(vertx: Vertx, private val config: Configuration) {
     fun deleteKey(context: RoutingContext) {
         val key = context.pathParam("key")
         redisClient?.run {
-            rxDel(key)
-                    .doOnSuccess { context.response().end() }
-                    .doOnError { context.fail(INTERNAL_SERVER_ERROR.code()) }
-                    .subscribe()
+            rxDel(key).subscribe(
+                    { context.response().end() },
+                    { logger.error("unable delete key", it); context.fail(INTERNAL_SERVER_ERROR.code()) }
+            )
         }
     }
 
+
     fun close(completer: Handler<AsyncResult<Void>>?) {
         redisClient?.close(completer)
+    }
+
+    private fun getKey(key: String): Single<JsonObject> {
+        return redisClient?.let {
+            it.rxGet(key)
+                    .flatMap {
+                        if (it.isNullOrEmpty()) Single.error(RuntimeException("no such key: $key"))
+                        else Single.just(JsonObject(it))
+                    }
+        } ?: Single.error(RuntimeException("no client available"))
     }
 
     private class DuplicateKeyException : RuntimeException()
